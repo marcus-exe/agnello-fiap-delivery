bd6607644568b9e85cea7d944d326de4d38cd0f8
Marcus Sena <mens.eai20@uea.edu.br>
2025-04-10 04:59:31 -0400

    Change port from 80 to 8080 for Security Issues

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ podman stop my-jsp-app 2>/dev/null || true
 podman rm my-jsp-app 2>/dev/null || true
 
 echo "🚀 Starting new container..."
-podman run -d --name my-jsp-app -p 80:8080 my-jsp-app:latest
+podman run -d --name my-jsp-app -p 8080:8080 my-jsp-app:latest
 
 echo "✅ App is live at http://54.198.38.100/"
